3e0c4100047e5611241e148280711914d539a9f6
Linda Casallas <lmcasallasa@libertadores.edu.co>
2025-05-22 01:31:06 +0000

    base

---
+++ script.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+    background(255);
+}
+
+function draw() {
+    // El contenido se agregará más adelante
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+    background(255);
+}
